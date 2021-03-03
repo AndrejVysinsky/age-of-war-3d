bc12b7e41be7904c00c79582848a78b9b536ce4a
Andrej Vyšinský <andrej.vysinsky@gmail.com>
2021-03-03 12:23:07 +0100

    added unity lines

---
+++ Age of War 3D/Assets/Scripts/Lines/Line.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour
+{
+    [SerializeField] List<GameObject> checkPoints;
+}
---
+++ Age of War 3D/Assets/Scripts/Lines/LineController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineController : MonoBehaviour
+{
+    [SerializeField] List<Line> lines;
+
+    public List<GameObject> LinePath { get; set; } = new List<GameObject>();
+    public List<Unit> UnitsInLine { get; set; } = new List<Unit>();
+}
