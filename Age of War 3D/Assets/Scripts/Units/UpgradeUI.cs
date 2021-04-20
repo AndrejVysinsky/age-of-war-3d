af287ea69010d1e58b0112212e2c7f18ea413f6f
Andrej Vyšinský <andrej.vysinsky@gmail.com>
2021-04-20 11:01:00 +0200

    added upgrade outpost tab to unit upgrades

---
+++ Age of War 3D/Assets/Scripts/Units/UpgradeUI.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeUI : MonoBehaviour
+{
+    [SerializeField] Button upgradeUnitsButton;
+    [SerializeField] Button upgradeOutpostButton;
+
+    [SerializeField] GameObject upgradeUnitsUI;
+    [SerializeField] GameObject upgradeOutpostUI;
+
+    [SerializeField] Color activeButtonColor;
+    [SerializeField] Color inactiveButtonColor;
+
+    private void Start()
+    {
+        upgradeUnitsButton.onClick.AddListener(ShowUnitUpgrades);
+        upgradeOutpostButton.onClick.AddListener(ShowOutpostUpgrades);
+    }
+
+    private void ShowOutpostUpgrades()
+    {
+        upgradeUnitsButton.image.color = inactiveButtonColor;
+        upgradeOutpostButton.image.color = activeButtonColor;
+
+        upgradeUnitsUI.SetActive(false);
+        upgradeOutpostUI.SetActive(true);
+    }
+
+    private void ShowUnitUpgrades()
+    {
+        upgradeUnitsButton.image.color = activeButtonColor;
+        upgradeOutpostButton.image.color = inactiveButtonColor;
+
+        upgradeUnitsUI.SetActive(true);
+        upgradeOutpostUI.SetActive(false);
+    }
+}
